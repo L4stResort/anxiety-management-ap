d64ae488877231f4701cdeb0fe7f02d6c4d285ad
L4stResort <99839117+L4stResort@users.noreply.github.com>
2025-11-04 11:01:39 -0500

    Change PORT to be a number from environment variable

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 // Puerto asignado por Render
-const PORT = process.env.PORT || 5000;
+const PORT = Number(process.env.PORT) || 5000;
 
 // Construir URI de MongoDB usando variables de entorno de Render
 const mongoUser = process.env.MONGO_USER;
