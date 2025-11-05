0aeab5fa7126e5527ede9765ea6dc78792f918bf
L4stResort <99839117+L4stResort@users.noreply.github.com>
2025-11-04 19:06:13 -0500

    Simplify next.config.js by removing experimental settings
    Removed experimental allowedDevOrigins setting.

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    // Permite cualquier origen en modo desarrollo
-    allowedDevOrigins: ['*']
-  }
+  reactStrictMode: true
 }
 
 module.exports = nextConfig;
